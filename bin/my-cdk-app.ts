91b8b5f16e782d9610a2b71128f95130e05c5b5a
MohammedAlkawari <mohamed.alkawari@iga.gov.bh>
2025-08-03 12:39:26 +0300

    Update my-cdk-app.ts

--- bin/my-cdk-app.ts
+++ bin/my-cdk-app.ts
@@ -1,1 +1,9 @@
-// my-cdk-app.ts placeholder
+#!/usr/bin/env node
+import * as cdk from "aws-cdk-lib";
+import { MyCdkStack } from "../lib/my-cdk-app-stack";
+
+// 💡 Initialize CDK App
+const app = new cdk.App();
+
+// 🚀 Deploy your main CDK stack
+new MyCdkStack(app, "MyCdkAppStack");
