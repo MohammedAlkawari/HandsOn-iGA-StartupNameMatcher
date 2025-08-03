9c3549dda6b5570991157a5e982568474ab518a2
MohammedAlkawari <mohamed.alkawari@iga.gov.bh>
2025-08-03 12:30:38 +0300

    Update my-cdk-app-stack.ts

--- lib/my-cdk-app-stack.ts
+++ lib/my-cdk-app-stack.ts
@@ -1,1 +1,43 @@
-// my-cdk-app-stack.ts placeholder
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as path from 'path';
+
+export class StartupNameMatcherStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // 1️⃣ Create S3 bucket to host the frontend
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    // 2️⃣ Create CloudFront distribution for the S3 bucket
+    const distribution = new cloudfront.Distribution(this, 'WebsiteDistribution', {
+      defaultBehavior: {
+        origin: new origins.S3Origin(websiteBucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+    });
+
+    // 3️⃣ Deploy frontend files from /frontend to the S3 bucket
+    new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+      sources: [s3deploy.Source.asset(path.join(__dirname, '..', 'frontend'))],
+      destinationBucket: websiteBucket,
+      distribution,
+      distributionPaths: ['/*'],
+    });
+
+    // 4️⃣ Output CloudFront domain
+    new cdk.CfnOutput(this, 'CloudFrontURL', {
+      value: `https://${distribution.domainName}`,
+      description: 'The CloudFront distribution URL',
+    });
+  }
+}
